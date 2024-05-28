495122c723635affcdd9c1821ac09db63d1d2f70
khubaib-gullo <166051982+khubaib-gullo@users.noreply.github.com>
2024-05-28 16:01:29 +0500

    Update middleware.py

--- myapp/middleware.py
+++ myapp/middleware.py
@@ -11,13 +11,16 @@ class IpBlocker:
         # One-time configuration and initialization.
 
     def __call__(self, request):
-        #
+        # blocking ip by port no
+        
         # add = request.META['REMOTE_ADDR']+":"+request.META['SERVER_PORT']+request.META['PATH_INFO']
         # print(add)
         # if add == "127.0.0.1:8000/home/":
         #     raise PermissionDenied()
         # response = self.get_response(request)
-        #
+        
+        # bloking ip by adding list of bannedips in  settings.py file
+        
         # if hasattr(settings, 'BANNED_IPS') and settings.BANNED_IPS is not None:
         #     if request.META['REMOTE_ADDR'] in settings.BANNED_IPS:
         #         raise PermissionDenied()
@@ -27,8 +30,8 @@ class IpBlocker:
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         # Check if the user is blocked
-        print(type(request.user))
-        print(type(view_func.__name__))
+       # print(type(request.user))
+       # print(type(view_func.__name__))
         if request.user.username == 'jhon' and view_func.__name__ == "home":
             return HttpResponseForbidden("You are currently blocked from accessing this application.")
 
